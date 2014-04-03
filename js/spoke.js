8a198012f4e46d32d2ca1203d1e185590a5447f1
Lawrence Naylor <naylor@larrypad.(none)>
2014-04-03 16:38:47 -0700

    basic working spoke calc

--- js/spoke.js
+++ js/spoke.js
@@ -1,9 +1,22 @@
 calculator = function() {
-    erd = Number($('input[name=erd]').val())
-    hub_flange = Number($('input[name=hub_flange]').val())
-    answer = erd + hub_flange
+    r1 = Number($('input[name=hub_flange]').val())/2
+    r2 = Number($('input[name=erd]').val())/2
+    r3 = 1.2
+    d = Number($('input[name=center_to_flange]').val())
+    m = Number($('input[name=number_holes]').val())/2
+    k = Number($('input[name=number_crosses]').val())
+    a = 360 * k / m
+    //a = 720 * k * Math.PI / 180 * m
+    console.log([r1,r2,r3,d,m,k,a]) 
+    
+    part1 = d*d + r1*r1 + r2*r2
+    part2 = 2 * r1 * r2 * Math.cos(a * Math.PI / 180)
+    part3 = part1 - part2 
+    answer = Math.sqrt(part3) 
+       
+    //answer = Math.sqrt(d*d + r1*r1 + r2*r2 - 2 * r1 * r2 * Math.cos(a)) - r3
 
-    alert(answer)
+    console.log(answer)
 
     return true
 }
