d9d7ba322d93db7c79b9f32315eae0a1fc7cbc6d
Ingy döt Net <ingy@ingy.net>
2014-04-03 14:22:06 -0700

    Add basic logic for getting data and calculating

--- js/spoke.js
+++ js/spoke.js
@@ -1,1 +1,16 @@
-alert(jQuery().jquery)
+calculator = function() {
+    erd = Number($('input[name=erd]').val())
+    hub_flange = Number($('input[name=hub_flange]').val())
+    answer = erd + hub_flange
+
+    alert(answer)
+
+    return true
+}
+
+setup = function() {
+    $('input[name=calc]').click(calculator)
+}
+
+// Magic line to load everything after page is ready:
+$(document).ready(setup)
